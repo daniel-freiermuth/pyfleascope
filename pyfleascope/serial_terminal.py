d0c69a949877099a6b5c9a2e332d92ff636e1e5f
Daniel Freiermuth <d_freiermu14@cs.uni-kl.de>
2025-05-25 22:20:40 +0200

    ref: Make fields in terminal private

--- pyfleascope/serial_terminal.py
+++ pyfleascope/serial_terminal.py
@@ -2,26 +2,26 @@ import serial
 
 class SerialTerminal:
     def __init__(self, port: str, baudrate: int = 9600, prompt: str = '>'):
-        self.serial = serial.Serial(port, baudrate)
-        self.port = port
-        self.baudrate = baudrate
-        self.prompt = prompt
-        self.serial.timeout = 0
-        self.serial.read_all()
+        self._serial = serial.Serial(port, baudrate)
+        self._port = port
+        self._baudrate = baudrate
+        self._prompt = prompt
+        self._serial.timeout = 0
+        self._serial.read_all()
 
     def exec(self, command: str, timeout: float | None = None):
-        self.serial.write((command + "\n").encode())
-        self.serial.timeout = timeout
-        response = self.serial.read_until(self.prompt.encode()).decode()
-        if response[-2:] != self.prompt:
-            raise TimeoutError(f"Expected prompt '{self.prompt}' but got '{response[-2:]}'. Likely due to a timeout.")
+        self._serial.write((command + "\n").encode())
+        self._serial.timeout = timeout
+        response = self._serial.read_until(self._prompt.encode()).decode()
+        if response[-2:] != self._prompt:
+            raise TimeoutError(f"Expected prompt '{self._prompt}' but got '{response[-2:]}'. Likely due to a timeout.")
         return response[:-2].strip()
     
     def send_ctrl_c(self):
-        self.serial.write(b'\x03')
+        self._serial.write(b'\x03')
 
     def send_reset(self):
-        self.serial.write(b'reset\n')
+        self._serial.write(b'reset\n')
     
     def __del__(self):
-        self.serial.close()
+        self._serial.close()
